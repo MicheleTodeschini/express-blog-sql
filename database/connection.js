07719a6c04ebcda38cb87aafee0b0a25a97dc877
MicheleTodeschini <micheletodeschini01@gmail.com>
2025-12-09 11:38:27 +0100

    add sql file and database name

--- database/connection.js
+++ database/connection.js
@@ -3,5 +3,9 @@ const mysql2 = require('mysql2')
 const connection = mysql2.connection({
     host: 'localhost',
     user: 'root',
-    password: 'root'
+    password: 'root',
+    database: 'blog'
 })
+
+
+module.exports = connection
